e4dde0f802facc37a7a670991ad22784361f6003
Jaqueline Fernandes de Andrade <jaqueline.desenvolvedora@gmail.com>
2026-09-12 16:24:56 -0300

    test: add Firefox and WebKit Playwright projects

--- ui-tests/playwright.config.ts
+++ ui-tests/playwright.config.ts
@@ -36,6 +36,18 @@ export default defineConfig({
       use: {
         ...devices['Desktop Chrome']
       }
+    },
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox']
+      }
+    },
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari']
+      }
     }
   ],
 
